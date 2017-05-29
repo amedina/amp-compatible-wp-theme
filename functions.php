1062c4894a83661105293577799cff8920ca458a
Alberto Medina <albertomedina@google.com>
2017-05-28 21:24:11 -0700

    1. Configure basic theme info (_style.scss) 2. Enqueue basic fonts 3. Make fonts responsive 4. Add preconnect for Google fonts 5. Get started with resposive layouts

--- functions.php
+++ functions.php
@@ -39,6 +39,9 @@ function amp_setup() {
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * This is what is referred now to as Featured Images.
+	 * If we want our theme to support adding feature images to posts and pages,
+	 * this feature has to be explicitly supported.
 	 */
 	add_theme_support( 'post-thumbnails' );
 
@@ -67,9 +70,70 @@ function amp_setup() {
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Add theme support for AMP
+	add_theme_support( 'amp' );
 }
 endif;
 add_action( 'after_setup_theme', 'amp_setup' );
+
+/**
+ * Register custom fonts.
+ */
+function amp_fonts_url() {
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Source Pro Sans font: on or off', 'amp' );
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'amp' );
+	$font_families = array();
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,700,900';
+	}
+	if ( 'off' !== $pt_serif ) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+	}
+
+	if (in_array( "on", array($source_sans_pro, $pt_serif) ) ) {
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function amp_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'amp-theme-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+/*
+ * The wp_resource_hints is the place where all the resource
+ * hints are given to the browser
+ */
+add_filter( 'wp_resource_hints', 'amp_resource_hints', 10, 2 );
+
+
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -105,15 +169,18 @@ add_action( 'widgets_init', 'amp_widgets_init' );
  * Enqueue scripts and styles.
  */
 function amp_scripts() {
+	// enqueue Google Fonts: PT Serif, Sans Pro
+	wp_enqueue_style('amp-theme-fonts',amp_fonts_url());
+
 	wp_enqueue_style( 'amp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'amp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'amp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+//	wp_enqueue_script( 'amp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+//
+//	wp_enqueue_script( 'amp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+//
+//	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+//		wp_enqueue_script( 'comment-reply' );
+//	}
 }
 add_action( 'wp_enqueue_scripts', 'amp_scripts' );
 
