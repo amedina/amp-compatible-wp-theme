1a12478ca3a8a59ab52687cbc9bd9bd308a2b216
Alberto Medina <albertomedina@google.com>
2017-06-05 19:52:04 -0700

    Add build-mode option to Customizer

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -13,8 +13,10 @@
 	<header class="entry-header">
 		<?php amp_the_category_list(); ?>
         <?php
+        $build_mode = get_theme_mod( 'markup');
+        $before = '<h1 class="entry-title"><span style="color:red">Generating ' . $build_mode . ' HTML</span>';
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( $before, '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
