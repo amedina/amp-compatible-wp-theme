d1e3d324c0bf0f7ec2a54064ca098bb64203f697
Alberto Medina <albertomedina@google.com>
2017-06-06 17:10:48 -0700

    Style comments section

--- comments.php
+++ comments.php
@@ -35,6 +35,16 @@ if ( post_password_required() ) {
 			?>
 		</h2><!-- .comments-title -->
 
+        <ol class="comment-list">
+			<?php
+			wp_list_comments( array(
+				'style'      => 'ol',
+				'short_ping' => true,
+				'avatar_size'=> '96',
+			) );
+			?>
+        </ol><!-- .comment-list -->
+
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'amp' ); ?></h2>
@@ -46,15 +56,6 @@ if ( post_password_required() ) {
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
-
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
-			?>
-		</ol><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
@@ -68,9 +69,7 @@ if ( post_password_required() ) {
 		</nav><!-- #comment-nav-below -->
 		<?php
 		endif; // Check for comment navigation.
-
 	endif; // Check for have_comments().
-
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -43,7 +43,7 @@
         <div class="entry-content">
 		    <?php
 		    the_content( sprintf(
-		    /* translators: %s: Name of current post. */
+		        /* translators: %s: Name of current post. */
 			    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'amp' ), array( 'span' => array( 'class' => array() ) ) ),
 			    the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		    ) );
